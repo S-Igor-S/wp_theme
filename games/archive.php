1648ca70f7ceb47d54920a36de83ac2093684511
Ihor <ihor.prachuk@gmail.com>
2020-11-23 14:10:41 +0200

    development

--- games/archive.php
+++ games/archive.php
@@ -11,6 +11,7 @@
 		<div>
 		<p>Release year: <?=get_post_meta(get_the_id(), 'release_year', true)?></p>
 		<p>Game genre: <?=get_post_meta(get_the_id(), 'game_genre', true)?></p>
+		<p>Developer: <?=implode(wp_get_post_terms(get_the_id(), 'developer_taxonomy', ['fields' => 'names']))?></p>
 	    </div>
 		<hr>
         <?php
